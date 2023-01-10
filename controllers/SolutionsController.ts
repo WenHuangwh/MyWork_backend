45f0d22b312b1380cf917c09e10883d602ebea61
wenhuang1 <huangwen@login.khoury.neu.edu>
2023-01-09 19:17:35 -0500

    all basic functions are implemented

--- controllers/SolutionsController.ts
+++ controllers/SolutionsController.ts
@@ -2,10 +2,12 @@ import { Request, Response } from "express";
 import SolutionsControllerI from "../interfaces/SolutionsControllerI";
 import SolutionsDao from "../daos/SolutionsDao";
 import Solutions from "../models/solutions/Solutions";
+import LeetcodesDao from "../daos/LeetcodesDao";
 
 export default class SolutionsController implements SolutionsControllerI {
 
     private static solutionsDao: SolutionsDao = SolutionsDao.getInstance();
+    private static leetcodesDao: LeetcodesDao = LeetcodesDao.getInstance();
     private static solutionsController: SolutionsController | null = null;
 
     public static getInstance = (app: any): any => {
@@ -28,18 +30,37 @@ export default class SolutionsController implements SolutionsControllerI {
     findSolutionByLid = (req: Request, res: Response) => {
         SolutionsController.solutionsDao.getSolutionsByLid(req.params.lid)
             .then((solutions: Solutions[]) => res.json(solutions));
-
     }
 
     createSolution = (req: Request, res: Response) => {
+        // const userId = req.body.uid;
+        // const importance = req.body.ratingImportance || 2.5;
+        // const repeat = req.body.ratingRepeat || 2.5;
+        // SolutionsController.leetcodesDao.getLeetcodesByID(req.body.leetcode_id)
+        //     .then(leetcode => {
+        //         leetcode.importance = ({ ...leetcode.importance, userID: importance });
+        //         leetcode.repeat = ({ ...leetcode.repeat, userID: repeat });
+        //         SolutionsController.leetcodesDao.updateLeetcode(leetcode._id, leetcode);
+        //     });
         SolutionsController.solutionsDao.createSolution(req.body)
             .then((solution: Solutions) => res.json(solution));
 
     }
 
     updateSolution = (req: Request, res: Response) => {
+        // const userId = req.body.uid;
+        // const importance = req.body.ratingImportance || 2.5;
+        // const repeat = req.body.ratingRepeat || 2.5;
+        // SolutionsController.leetcodesDao.getLeetcodesByID(req.body.leetcode_id)
+        //     .then(leetcode => {
+        //         leetcode.importance = ({ ...leetcode.importance, userID: importance });
+        //         leetcode.repeat = ({ ...leetcode.repeat, userID: repeat });
+        //         SolutionsController.leetcodesDao.updateLeetcode(leetcode._id, leetcode);
+        //     });
         SolutionsController.solutionsDao.updateSolution(req.params.sid, req.body)
             .then((solution: Solutions) => res.json(solution));
     }
+
+
 
 }
