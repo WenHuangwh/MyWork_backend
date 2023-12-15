ed4f0674aa29ab7bdabda63f244cfa6e8ac4afac
wenhuang1 <huangwen@login.khoury.neu.edu>
2023-12-15 15:59:39 +0800

    New job page

--- models/jobs/Jobs.ts
+++ models/jobs/Jobs.ts
@@ -1,10 +1,5 @@
 export default interface Jobs {
     company: String,
-    term: String,
-    jobType: String,
     link: String,
-    recommendation: Number,
-    date: Date,
-    author: String,
     comment: String
 };
