27791ad05d50a2e2e64f9562a84d64fb673d7ce3
wenhuang1 <huangwen@login.khoury.neu.edu>
2024-07-17 22:06:15 -0700

    Update job schema

--- mongoose/jobs/JobsSchema.ts
+++ mongoose/jobs/JobsSchema.ts
@@ -3,12 +3,7 @@ import Jobs from "../../models/jobs/Jobs";
 
 const JobsSchema = new mongoose.Schema<Jobs>({
     company: { type: String },
-    term: { type: String },
-    jobType: { type: String },
     link: { type: String },
-    recommendation: { type: Number },
-    date: { type: Date, default: new Date() },
-    author: { type: String },
     comment: { type: String }
 }, { collection: "jobs" });
 export default JobsSchema;
